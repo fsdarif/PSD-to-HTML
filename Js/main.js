77ff2934830a2e12af29a95d59f4cf55a4968518
Ahnaf Arif <2080arif@gmail.com>
2021-11-20 16:50:03 +0600

    added

---
+++ Js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(function(){
+        $('.slider').bxSlider({
+          mode: 'fade',
+          captions: true,
+          slideWidth: 940
+        });
+      });
+  });
+
